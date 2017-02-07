3a11869948d3f05c285ed15172eb1349ef75eb72
Mathias Rav <rav@cs.au.dk>
2017-02-07 13:31:31 +0100

    WIP

--- imagetktest.py
+++ imagetktest.py
@@ -1,33 +1,6 @@
-from functools import partial, wraps
-
-import numpy as np
 import tkinter
-import PIL.Image
-import PIL.ImageTk
-import cairo
 
-
-def save_restore(fn):
-    @wraps(fn)
-    def wrapper(self, *args, **kwargs):
-        self.context.save()
-        result = fn(self, *args, **kwargs)
-        self.context.restore()
-        return result
-
-    return wrapper
-
-
-def draw_history(fn):
-    fn = save_restore(fn)
-
-    @wraps(fn)
-    def wrapper(self, *args, **kwargs):
-        self.draw_history.append(partial(fn, self, *args, **kwargs))
-        self.draw_history[-1]()
-        self.paste()
-
-    return wrapper
+from surface import Surface
 
 
 class ExampleGui(tkinter.Tk):
@@ -56,36 +29,13 @@ class ExampleGui(tkinter.Tk):
         w, h = ev.width, ev.height
         if (cur_w, cur_h) != (w, h):
             self.surface.resize(w, h)
-            self.surface.pack(expand=True, fill="both")
-            self.surface.redraw()
 
     def on_release(self, ev):
         assert ev.type == tkinter.EventType.ButtonRelease, ev
 
-    @draw_history
-    def circle_device_radius(self, x, y, device_radius):
-        x, y = self.context.user_to_device(x, y)
-        self.context.identity_matrix()
-        self.context.translate(x, y)
-        self.context.arc(0, 0, device_radius, 0, 2*np.pi)
-        self.context.set_source_rgba(1, 0, 0, 0.8)
-        self.context.fill()
-
-    def _poly(self, *points):
-        self.context.move_to(*points[0])
-        for p in points[1:]:
-            self.context.line_to(*p)
-        self.context.close_path()
-        self.context.set_source_rgba(1, 0, 0, 0.8)
-        self.context.fill()
-
-    @draw_history
-    def poly(self, *points):
-        self._poly(*points)
-
     def on_press(self, ev):
         assert ev.type == tkinter.EventType.ButtonPress, ev
-        x, y = self.context.device_to_user(ev.x, ev.y)
+        x, y = self.surface.device_to_user(ev.x, ev.y)
         try:
             method = getattr(self, 'on_press_%s' % ev.num)
         except AttributeError:
@@ -94,23 +44,21 @@ class ExampleGui(tkinter.Tk):
             method(x, y)
 
     def on_press_1(self, x, y):
-        self.circle_device_radius(x, y, 20)
+        self.surface.circle_device_radius(x, y, 20)
         self.points.append((x, y))
-        if len(self.points) == 3:
-            self.poly(*self.points)
-            del self.points[:]
+
+    def on_press_3(self, x, y):
+        self.surface.circle_device_radius(x, y, 20)
+        self.points.append((x, y))
+        if len(self.points) >= 3:
+            self.surface.poly(*self.points)
+        del self.points[:]
 
     def on_press_4(self, x, y):
-        self.context.translate(x, y)
-        self.context.scale(self.SCROLL_SCALE, self.SCROLL_SCALE)
-        self.context.translate(-x, -y)
-        self.redraw()
+        self.surface.zoom(x, y, self.SCROLL_SCALE)
 
     def on_press_5(self, x, y):
-        self.context.translate(x, y)
-        self.context.scale(1/self.SCROLL_SCALE, 1/self.SCROLL_SCALE)
-        self.context.translate(-x, -y)
-        self.redraw()
+        self.surface.zoom(x, y, 1/self.SCROLL_SCALE)
 
 
 if __name__ == "__main__":
--- surface.py
+++ surface.py
@@ -1,5 +1,37 @@
+from functools import partial, wraps
+
+import numpy as np
+import tkinter
+import PIL.Image
+import PIL.ImageTk
+import cairo
+
+
+def save_restore(fn):
+    @wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        self.context.save()
+        result = fn(self, *args, **kwargs)
+        self.context.restore()
+        return result
+
+    return wrapper
+
+
+def draw_history(fn):
+    fn = save_restore(fn)
+
+    @wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        self.draw_history.append(partial(fn, self, *args, **kwargs))
+        self.draw_history[-1]()
+        self.update()
+
+    return wrapper
+
+
 class Surface:
-    def __init__(self, parent, width, height):
+    def __init__(self, parent, w, h):
         self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, w, h)
         self.context = cairo.Context(self.surface)
 
@@ -21,24 +53,34 @@ class Surface:
         return self.surface.get_height()
 
     def resize(self, w, h):
-        cur_w, cur_h = width, height
         cur_x0 = self.context.device_to_user(0, 0)[0]
-        cur_x1 = self.context.device_to_user(cur_w, 0)[0]
+        cur_x1 = self.context.device_to_user(self.width, 0)[0]
+        cur_x = cur_x0 + (cur_x1 - cur_x0) / 2
         cur_y0 = self.context.device_to_user(0, 0)[1]
-        cur_y1 = self.context.device_to_user(0, cur_h)[1]
-        new_scale = min(h / (cur_y1 - cur_y0), w / (cur_x1 - cur_x0))
+        cur_y1 = self.context.device_to_user(0, self.height)[1]
+        cur_y = cur_y0 + (cur_y1 - cur_y0) / 2
+        # new_scale = min(h / (cur_y1 - cur_y0), w / (cur_x1 - cur_x0))
+        new_scale = (h / (cur_y1 - cur_y0) if self.height < self.width else
+                     w / (cur_x1 - cur_x0))
+        new_user_width = w / new_scale
+        new_user_height = h / new_scale
 
         self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, w, h)
+        self.context.identity_matrix()
         self.context = cairo.Context(self.surface)
-        self.context.translate(cur_x0, cur_y0)
         self.context.scale(new_scale, new_scale)
+        self.context.translate(-cur_x, -cur_y)
+        self.context.translate(new_user_width / 2, new_user_height / 2)
         self._image_ref = PIL.ImageTk.PhotoImage('RGBA', (w, h))
         self.label.configure(image=self._image_ref)
+        self.pack(expand=True, fill="both")
+        self.redraw()
 
     def update(self):
         w, h = self.surface.get_width(), self.surface.get_height()
         surface_data = self.surface.get_data().obj
-        img = PIL.Image.frombytes("RGBA", (w, h), surface_data, "raw", "BGRA", 0, 1)
+        img = PIL.Image.frombytes("RGBA", (w, h), surface_data,
+                                  "raw", "BGRA", 0, 1)
         self._image_ref.paste(img)
 
     def redraw(self):
@@ -49,3 +91,33 @@ class Surface:
         for call in self.draw_history:
             call()
         self.update()
+
+    @draw_history
+    def circle_device_radius(self, x, y, device_radius):
+        x, y = self.context.user_to_device(x, y)
+        self.context.identity_matrix()
+        self.context.translate(x, y)
+        self.context.arc(0, 0, device_radius, 0, 2*np.pi)
+        self.context.set_source_rgba(1, 0, 0, 0.8)
+        self.context.fill()
+
+    def _poly(self, *points):
+        self.context.move_to(*points[0])
+        for p in points[1:]:
+            self.context.line_to(*p)
+        self.context.close_path()
+        self.context.set_source_rgba(1, 0, 0, 0.8)
+        self.context.fill()
+
+    @draw_history
+    def poly(self, *points):
+        self._poly(*points)
+
+    def zoom(self, x, y, f):
+        self.context.translate(x, y)
+        self.context.scale(f, f)
+        self.context.translate(-x, -y)
+        self.redraw()
+
+    def device_to_user(self, x, y):
+        return self.context.device_to_user(x, y)
