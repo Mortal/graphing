d8e1015fc14249d3e1d749b1eaaea716f53f8842
Mathias Rav <rav@cs.au.dk>
2017-02-08 11:36:08 +0100

    Regenerate context.py

--- cairoshim/context.py
+++ cairoshim/context.py
@@ -1,23 +1,5 @@
 from cairo import Context as _Context
-
-
-def superclass_of(t):
-    def subclasscheck(cls, subclass):
-        if subclass is t:
-            return True
-        return super(superclass_of_t, cls).__subclasscheck__(subclass)
-
-    def instancecheck(cls, instance):
-        if type(instance) is t:
-            return True
-        return super(superclass_of_t, cls).__instancecheck__(instance)
-
-    superclass_of_t = type(
-        'superclass_of_%s' % t.__name__, (type,),
-        dict(__subclasscheck__=subclasscheck,
-             __instancecheck__=instancecheck))
-
-    return superclass_of_t
+from .metamagic import superclass_of
 
 
 class Context(_Context, metaclass=superclass_of(_Context)):
